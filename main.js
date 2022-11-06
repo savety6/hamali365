42bfd7a18cf5d33efc2b3005bfe920247cca5a1f
savety6 <vladimirkucev6@gmail.com>
2022-11-06 10:53:06 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,25 @@
+const hiddenElements = document.querySelectorAll('.hidden')
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show')
+        }else{
+            entry.target.classList.remove('show')
+        }
+    })
+},{threshold: 1})
+
+
+//scroll event listener
+const header = document.querySelector('.navBar')
+window.addEventListener('scroll', () => {
+    if(window.scrollY > 0){
+        console.log(window.scrollY);
+        header.classList.add('sticky')
+    }else{
+        header.classList.remove('sticky')
+    }
+})
+
+hiddenElements.forEach((element) => {observer.observe(element)})
